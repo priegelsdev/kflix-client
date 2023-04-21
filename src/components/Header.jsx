d984fd8999e3c83ca0adae74046dabb278a25bab
priegelsdev <priegelsdev@gmail.com>
2023-04-21 21:54:28 +0200

    update header with bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 import { Link, NavLink } from "react-router-dom"
 import logo from '../img/logo.png'
 
+// react-bootstrap
+import { Container, Nav, Navbar, Button, Image } from "react-bootstrap"
+
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">
-          <img className="logo" src={logo}/>
-        </Link>
-        <ul className="nav-links">
-          <NavLink>Home</NavLink>
-          <NavLink>Profile</NavLink>
-          <NavLink>Logout</NavLink>
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="light" variant="light">
+    <Container fluid>
+      <Navbar.Brand href="#home">
+        <Image className="logo rounded border border-dark" src={logo} />
+      </Navbar.Brand>
+      <Nav className="me-auto gap-3">
+        <Nav.Link className="nav-link" href="#home">Home</Nav.Link>
+        <Nav.Link className="nav-link" href="#features">Profile</Nav.Link>
+        <Button className="btn btn-danger">login/logout</Button>
+      </Nav>
+    </Container>
+  </Navbar>
   )
 }
