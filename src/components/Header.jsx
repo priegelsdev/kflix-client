0935a29e4416a38f16f98d4ae858f264dddd4b97
priegelsdev <priegelsdev@gmail.com>
2023-04-21 14:49:44 +0200

    create basic header with style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import logo from '../img/logo.png'
 
 export default function Header() {
   return (
     <header>
       <nav>
-        <NavLink>
-          Profile
-        </NavLink>
+        <Link to="/">
+          <img className="logo" src={logo}/>
+        </Link>
+        <ul className="nav-links">
+          <NavLink>Home</NavLink>
+          <NavLink>Profile</NavLink>
+          <NavLink>Logout</NavLink>
+        </ul>
       </nav>
     </header>
   )
