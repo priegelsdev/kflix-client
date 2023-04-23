0e3b5daff9459bc827db3fd0adcde3eb65933621
priegelsdev <priegelsdev@gmail.com>
2023-04-23 11:43:45 +0200

    create grid system for movie cards

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,10 +1,48 @@
 import MovieCard from "./MovieCard";
-
+import movies from "../../data";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function MainView() {
+ 
+  const movieCardElements = <Container>
+    <Row>
+      {movies.slice(0, 4).map((movie, index) => {
+        return <Col sm={3} key={index}>
+          <MovieCard 
+            key = {movie.id}
+            title = {movie.title}
+            description = {movie.description}
+          />
+        </Col>
+      })}
+    </Row>
+    <Row>
+      {movies.slice(4, 8).map((movie, index) => {
+        return <Col sm={3} key={index}>
+          <MovieCard 
+            key = {movie.id}
+            title = {movie.title}
+            description = {movie.description}
+          />
+        </Col>
+      })}
+    </Row>
+    <Row>
+      {movies.slice(8, 10).map((movie, index) => {
+        return <Col sm={6} key={index}>
+          <MovieCard 
+            key = {movie.id}
+            title = {movie.title}
+            description = {movie.description}
+          />
+        </Col>
+      })}
+    </Row>
+  </Container>
+
   return (
-    <>
-      <MovieCard />
-    </>
+    <Container>
+      {movieCardElements}
+    </Container>
   )
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import { Card } from "react-bootstrap"
 
-export default function MovieCard() {
+export default function MovieCard(props) {
   return (
     <Card>
-      <Card.Img variant="top" src="https://placehold.co/50x20"/>
+      <Card.Img variant="top" src="https://placehold.co/400x50"/>
       <Card.Body>
-        <Card.Title>Movie Title</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Description of the movie
+          {props.description}
         </Card.Text>
         <a href="#">Open</a>
       </Card.Body>
